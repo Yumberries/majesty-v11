456120bc81661d84e65d96cea9132f898af4d10a
Yumberries <67992156+Yumberries@users.noreply.github.com>
2024-12-01 09:51:37 -0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,31 @@
+function set_Map (Map: number) {
+    Maps = [tilemap`level8`, tilemap`level9`]
+    tiles.setCurrentTilemap(Maps[Map - 1])
+}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    set_Map(2)
 })
-tiles.setCurrentTilemap(tilemap`level2`)
+let Maps: tiles.TileMapData[] = []
+set_Map(1)
 let mySprite = sprites.create(img`
+    1 1 1 1 . . 6 6 6 6 . . 1 1 1 1 
+    1 . . 1 . . 6 . . 6 . . 1 . . 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 1 . . . . . . . . . . . . 1 1 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
+    6 6 . . . . . . . . . . . . 6 6 
+    6 . . . . . . 6 6 . . . . . . 6 
+    6 . . . . . . 6 6 . . . . . . 6 
+    6 6 . . . . . . . . . . . . 6 6 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    1 1 . . . . . . . . . . . . 1 1 
+    1 . . . . . . . . . . . . . . 1 
+    1 . . 1 . . 6 . . 6 . . 1 . . 1 
+    1 1 1 1 . . 6 6 6 6 . . 1 1 1 1 
     `, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
-controller.moveSprite(mySprite)
+grid.snap(mySprite)
+grid.moveWithButtons(mySprite)
+info.setScore(500)
