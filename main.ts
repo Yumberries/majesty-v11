55bbfae0be6afb9784e02aaf37cbdac55daf8b7a
Yumberries <67992156+Yumberries@users.noreply.github.com>
2024-12-01 08:50:11 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+tiles.setCurrentTilemap(tilemap`level2`)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite)
